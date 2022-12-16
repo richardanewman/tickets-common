ea4bb2e75e6fbf7504d64ac51bda6bb9655f41bf
Rick Newman <rick@richardnewman.dev>
2022-12-16 12:47:39 -0500

    update exports

--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,9 @@ export * from './middlewares/current-user-token';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
-export * from './events';
+
+export * from './events/base-listener';
+export * from './events/base-publisher';
+export * from './events/subjects';
+export * from './events/ticket-created-event';
+export * from './events/ticket-updated-event';
