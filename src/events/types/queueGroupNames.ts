b5d5751fd554de1474851c4cc60259abb63db787
Rick Newman <rick@richardnewman.dev>
2023-02-11 11:40:05 -0500

    adds new group name

--- src/events/types/queueGroupNames.ts
+++ src/events/types/queueGroupNames.ts
@@ -2,4 +2,5 @@ export enum QueueGroupNames {
   ORDERS_SERVICE = 'orders-service',
   TICKETS_SERVICE = 'tickets-service',
   PAYMENTS_SERVICE = 'payment-service',
+  EXPIRATION_SERVICE = 'expiration-service',
 }
